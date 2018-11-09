0ced35c386ff9420e1ce17e8dac6c739496b43e5
Rajashow Parajuli <rajashowparajuli@gmail.com>
2018-11-09 12:48:39 -0500

    init view's states and views

---
+++ src/view/WashRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class WashRoom extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
